766a6ecdf13bd1f1a6d28d6b45254c3cc189e641
subfield <tetelainv@gmail.com>
2024-09-28 13:09:57 +0100

    fixed TS on admin

--- admin/src/config/index.ts
+++ admin/src/config/index.ts
@@ -3,22 +3,18 @@ import { SiteUrl } from "../const";
 export const CLIENT_ID = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
 export const SITE_MODE = import.meta.env.VITE_SITE_MODE;
 export const NETWORK_MODE = import.meta.env.VITE_NETWORK_MODE;
-export const CC_APIKEY = import.meta.env.VITE_CC_API_KEY;
-export const tokenAddress = import.meta.env.VITE_TOKENADDRESS;
-export const SS = import.meta.env.VITE_SITE_STAGE;
-
-const SITEURL = SITE_MODE === "live";
-
-const x = import.meta.env.VITE_LIVE_API;
+const TEST_NESTAGE = import.meta.env.VITE_TEST_NESTAGE;
+const LIVE_NESTAGE = import.meta.env.VITE_LIVE_NESTAGE;
 const TEST_API = import.meta.env.VITE_TEST_API;
-// const LIVE_API = x !== "" || x.length > 0 ? x : ""; //
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+// const x = import.meta.env.VITE_LIVE_API;
+// const LIVE_API = x !== "" || x.length > 0 ? x : "";
 
 let LIVE_API = "";
 
 const A = "https://application.nestage.io/api/v1/"; // Mainnet - 1
 // const LIVE_API = "https://nestage-server-main.onrender.com/api/v1/"; // Mainnet - 2
-const B = "https://api-testing.nestage.io/api/v1/"; // Testnet - 1
-// const LIVE_API = "https://nestage-server-testing.onrender.com/api/v1/"; // Testnet - 2
+const B = "https://api-testing.nestage.io/api/v1/"; // Testnet
 // const LIVE_API = "https://n-server-1lbk.onrender.com/api/v1/";
 
 if (SiteUrl.includes("testing") || SiteUrl.includes("localhost")) {
@@ -27,6 +23,8 @@ if (SiteUrl.includes("testing") || SiteUrl.includes("localhost")) {
   LIVE_API = A;
 }
 
+export const SITE_URL = SITE_MODE === "live" ? LIVE_NESTAGE : TEST_NESTAGE;
 export const API_URL = SITE_MODE === "live" ? LIVE_API : TEST_API;
 
+export const CC_APIKEY = import.meta.env.VITE_CC_API_KEY;
 export const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN;
